b3927f381280cb76ffbc7fce8ae2e184dbb2419b
JacobDFlores <jacobfdaniel@gmail.com>
2023-06-27 14:44:54 -0500

    updated files

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const inquirer = require('inquirer');
+const iMaxLength = require('inquirer-maxlength-input-prompt');
 const fs = require('fs');
 const shapes = require('./lib/shapes.js');
 
+const circleShape = new shapes.Circle;
+
+inquirer.registerPrompt('maxlength-input', iMaxLength)
 inquirer
   .prompt([
     {
-      type: 'input',
+      type: 'maxlength-input',
       message: 'Enter up to 3 characters.',
       name: 'logoTitle',
+      maxLength: 3,
     },
     {
       type: 'input',
@@ -31,14 +36,21 @@ inquirer
     switch(response.userShape){
         case 'Square':
             console.log('square is true');
+            let squareSvg = shape.Circle(response.logoTitle, response.titleColor, response.userShape, response.shapeColor).render();
+            fs.writeFile('./examples/logo.svg', squareSvg, (err) =>
+                err ? console.error(err) : console.log('Generated logo.svg'))
             break;
         case 'Circle':
             console.log('circle is true');
-
+            let circleSvg = circleShape(response.logoTitle, response.titleColor, response.userShape, response.shapeColor).render();
+            fs.writeFile('./examples/logo.svg', circleSvg, (err) =>
+                err ? console.error(err) : console.log('Generated logo.svg'))
             break;
         case 'Triangle':
             console.log('triangle is true');
-
+            let triangleSvg = shape.Circle(response.logoTitle, response.titleColor, response.userShape, response.shapeColor).render();
+            fs.writeFile('./examples/logo.svg', triangleSvg, (err) =>
+                err ? console.error(err) : console.log('Generated logo.svg'))
     }
 
   });
